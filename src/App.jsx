c07c5e14852e43b44ae095b43877728bb4e6b587
BenjaMura <benjaminmuratore1@gmail.com>
2023-09-30 11:00:29 -0300

    Cancel changes app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { Landing, Home, Detail, Form, NotFound, Activities, Edit } from "./views";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from './components/Footer/Footer';
-import { getActivities, getCountries } from './redux/actions';
 
 function App() {
   const location = useLocation();
-  const dispatch = useDispatch();
 
   const showComponents = () => {
     if (location.pathname === "/home" ||
@@ -22,11 +18,6 @@ function App() {
     return false
   };
   
-  useEffect(() => {
-    dispatch(getCountries());
-    dispatch(getActivities());
-  }, []);
-
   return (
     <div className="App">
       {showComponents() &&
